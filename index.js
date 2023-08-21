ef237d0e54ee57574192f7cc5540bad67e1eb183
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-08-22 02:49:10 +0530

    created router for product to get all products,get single product,search product,create new product

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const mongoose = require("mongoose");
 dotenv.config();
+const mongoose = require("mongoose");
+const productRoute = require("./routes/products");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT, () => console.log(`Server running...`));
--- models/Product.js
+++ models/Product.js
@@ -27,4 +27,4 @@ const ProductSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model();
+module.exports = mongoose.model("Product", ProductSchema);
